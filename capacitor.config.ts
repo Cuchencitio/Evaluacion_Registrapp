389177f45db35f61f5a6210ffe8bbb99f81fc4f1
Cuchencitio <cesarmaureira95@gmail.com>
2023-11-27 21:32:35 -0300

    Generacion apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Registrapp',
+  appName: 'CMAUREIRA',
   webDir: 'www',
   server: {
     androidScheme: 'https'
